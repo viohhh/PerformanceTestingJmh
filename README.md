2320e458d28cab0e5161cc8c2381039d965fe869
Paul156551 <pavel.ryzhkov.99@mail.ru>
2024-08-27 08:52:21 +0300

    Начало работы над сервисом с JMH

---
+++ README.md
@@ -0,0 +1,4 @@
+# jmhdemo
+
+
+Refer to: https://ihendracode.blogspot.com/2019/06/openjdkjmh-simple-usage.html
---
+++ src/main/java/com/sevnis/jmhdemo/Application.java
@@ -0,0 +1,13 @@
+package com.sevnis.jmhdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
